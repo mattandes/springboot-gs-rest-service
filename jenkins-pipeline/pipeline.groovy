e36abea53cbd2f4dee7ccf9c63009b9fb1819d3e
Matt Andes <m.andes@hotmail.com>
2018-10-05 22:15:21 -0500

    Adding Jenkins pipeline support.

---
+++ jenkins-pipeline/pipeline.groovy
@@ -0,0 +1,22 @@
+pipelineJob('springboot-gs-rest-service-build') {
+
+  def repo = 'https://github.com/mattandes/springboot-gs-rest-service.git'
+
+  triggers {
+    scm('H/5 * * * *')
+  }
+  description("SpringBoot GS Rest Service Build")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master')
+          scriptPath('jenkins-pipeline/Jenkinsfile')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+      }
+    }
+  }
+}
